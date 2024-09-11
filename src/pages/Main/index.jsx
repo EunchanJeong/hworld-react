d3b0d1713c788d0a85bd06d7aeae3307338b9b2c
YeongWook <ehdrnr16whduddnr@naver.com>
2024-09-11 09:26:04 +0900

    :sparkles: Feat: 헤더 컴포넌트 추가

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Sample } from './styled';
+import Header from '../../components/Header';
 
 const Main = () => {
   return (
     <>
+      <Header />
       <Sample>This is Sample</Sample>
     </>
   );
