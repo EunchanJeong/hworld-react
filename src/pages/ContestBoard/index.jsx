e9bdc93a896c18333912e83f280d87b5d47695f5
Eunchan Jeong <jec65147@gmail.com>
2024-09-12 13:50:12 +0900

    :sweat_drops: Refactor: 리액트 쿼리 적용

--- src/pages/ContestBoard/index.jsx
+++ src/pages/ContestBoard/index.jsx
@@ -5,30 +5,42 @@ import Button from '../../components/Button';
 import CoordinationPost from '../../components/CoordinationPost'; // 게시글 목록에 사용할 CoordinationPost 컴포넌트
 import { Container, FilterDiv, ButtonWrapper, PostListContainer, PaginationButton, StatusText } from './styled'; // 스타일 임포트
 import { GetContestPostListAPI } from '../../apis/Contest/ContestAPI';
+import { useQuery } from 'react-query';
+import Spinner from '../../components/Spinner';
 
 const POSTS_PER_PAGE = 8; // 한 페이지에 8개의 게시글 표시
 
 const ContestBoard = () => {
   const [activeTab, setActiveTab] = useState('latest'); // 정렬 기준 (최신순/추천순)
   const [contestStatus, setContestStatus] = useState('ongoing'); // 콘테스트 상태 (진행중/완료)
-  const [postList, setPostList] = useState([]); // 게시글 목록
+  // const [postList, setPostList] = useState([]); // 게시글 목록
   const [currentPage, setCurrentPage] = useState(1); // 현재 페이지
 
   // API 호출을 통해 게시글 목록을 가져오는 함수
-  const fetchPosts = async () => {
-    try {
-      const response = await GetContestPostListAPI(contestStatus, activeTab);
-      console.log(response.data);
-      setPostList(response.data); // 게시글 목록 상태 업데이트
-    } catch (error) {
-      console.error('게시글 목록 가져오기 오류:', error);
-    }
-  };
+  const {
+    data: postList = [],
+    isLoading,
+    isError,
+  } = useQuery(
+    ['posts', contestStatus, activeTab, currentPage], // queryKey로 contestStatus, activeTab, currentPage 사용
+    () => GetContestPostListAPI(contestStatus, activeTab, currentPage, POSTS_PER_PAGE), // 데이터를 가져오는 함수
+    {
+      keepPreviousData: true, // 페이지 전환 시 이전 데이터를 유지
+    },
+  );
 
-  // activeTab 또는 contestStatus가 변경될 때마다 API 호출
-  useEffect(() => {
-    fetchPosts();
-  }, [activeTab, contestStatus]);
+  // // activeTab 또는 contestStatus가 변경될 때마다 API 호출
+  // useEffect(() => {
+  //   fetchPosts();
+  // }, [activeTab, contestStatus]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <div>오류가 발생했습니다.</div>;
+  }
 
   // 페이지당 표시할 게시글 계산
   const startIndex = (currentPage - 1) * POSTS_PER_PAGE;
