6060c71ebb6e4481a36accd37ece538d56560668
wlgus253254 <wlgus253254@ewhain.net>
2024-09-12 19:53:50 +0900

    :sparkles: Feat: 마이페이지 내 코디 조회 구현

--- src/apis/Member/MyPageAPI.js
+++ src/apis/Member/MyPageAPI.js
@@ -3,11 +3,19 @@ import { axiosInstance } from '..';
 const MyPageAPI = {
   // 회원 정보 가져오기
   getMemberInfo: () => {
-    return axiosInstance.get('/members/my-info');
+    return axiosInstance.get(`/members/my-info`);
   },
   // 회원 게시글 목록 가져오기
   getMemberPost: (orderBy, page, size) => {
     return axiosInstance.get(`/members/my-posts?orderBy=${orderBy}&page=${page}&size=${size}`);
+  },
+  // 회원 코디 목록 가져오기
+  getMemberCoordination: () => {
+    return axiosInstance.get(`/members/my-coordinations`);
+  },
+  // 회원 코디에서 사용한 아이템 목록 가져오기
+  getMemberCoordinationItem: (coordinationId) => {
+    return axiosInstance.get(`/members/my-coordinations/${coordinationId}`);
   },
 };
 export default MyPageAPI;
